f4c367a375b297fd8f665ad8f3e05d0e6bca1e9e
Bo <fb8fb8@gmail.com>
2013-05-21 16:43:25 -0500

    1
    Signed-off-by: Bo <fb8fb8@gmail.com>

---
+++ src/testC.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : testC.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int p;
+	cout<<"input a value:"<<endl;
+	cin>>p;
+	cout<<p;
+	for(int i=0; i<3; i++)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+}
